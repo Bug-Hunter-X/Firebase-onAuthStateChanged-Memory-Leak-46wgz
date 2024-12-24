49beda46d77f4f340677b112ac20beafd6378d6a
BugHunter-X <askzenai@gmail.com>
2024-12-24 15:32:06 +0100

    Add authBug.js file

---
+++ authBug.js
@@ -0,0 +1,13 @@
+const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+// unsubscribe(); //this line was missing causing memory leak
